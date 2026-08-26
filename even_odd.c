bdb24d4a93a4b6f2d9f31837083f13ca922cbb48
nainshiy40-ydv <nainshiy40@gmail.com>
2026-08-26 23:26:54 +0530

    Create even_odd.c

---
+++ even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{  
+    int num;
+    
+    printf("enter the value of num:");
+    scanf("%d",&num);
+    if (num%2==0)
+    {
+        printf("even number");
+    }
+    else
+    {
+        printf("odd number");
+    }
+
+    return 0;
+}

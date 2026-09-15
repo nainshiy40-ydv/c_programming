3ce845e70eecb248412d12e1804929fc0db388ea
nainshiy40-ydv <nainshiy40@gmail.com>
2026-09-15 09:47:06 +0530

    Implement comparison and modification functions

---
+++ day32.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int a,b;
+void compare()
+{
+    if (a>b)
+    {
+        printf("%d is the largest\n",a);
+        printf("%d is smallest\n",b);
+    }
+    else if (a<b)
+    {
+        printf("%d is the largest\n",b);
+        printf("%d is the smallest\n",a);
+    }
+    else
+    {
+        printf("both are equal");
+    }
+}
+void modify()
+{
+    printf("inside modify() a=%d\n",a);
+    printf("inside modify() b=%d\n",b);
+    
+}
+int main()
+{
+    printf("enter two values:");
+    scanf("%d %d",&a,&b);
+    modify();
+    compare();
+    
+}

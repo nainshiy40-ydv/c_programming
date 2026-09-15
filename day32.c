ba52fe9a2353d8ae71df4ea892a401f14488afd2
nainshiy40-ydv <nainshiy40@gmail.com>
2026-09-15 09:51:21 +0530

    Implement modify function and update main logic
    Added a modify function to increment variables a and b, and updated main to call modify and compare functions.

--- day32.c
+++ day32.c
@@ -18,6 +18,34 @@ void compare()
     }
 }
 void modify()
+{ 
+    a= ++a;
+    b= ++b;
+    printf("inside modify() a=%d\n",a);
+    printf("inside modify() b=%d\n",b);
+    
+}
+int main()
+{#include <stdio.h>
+int a,b;
+void compare()
+{
+    if (a>b)
+    {
+        printf("%d is the largest\n",a);
+        printf("%d is smallest\n",b);
+    }
+    else if (a<b)
+    {
+        printf("%d is the largest\n",b);
+        printf("%d is the smallest\n",a);
+    }
+    else
+    {
+        printf("both are equal");
+    }
+}
+void modify()
 {
     printf("inside modify() a=%d\n",a);
     printf("inside modify() b=%d\n",b);
@@ -29,5 +57,11 @@ int main()
     scanf("%d %d",&a,&b);
     modify();
     compare();
+    
+}
+    printf("enter two values:");
+    scanf("%d %d",&a,&b);
+    compare();
+    modify();
     
 }
